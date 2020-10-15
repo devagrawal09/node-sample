e703011e505f10c51796c1152a7a8788ff8e4086
Dev Agrawal <devagrawal09@gmail.com>
2020-10-16 00:58:15 +0530

    fixed port binding

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 const isProduction = process.env.PRODUCTION === `true`;
+const port = process.env.PORT || 3000;
 
 // Jade
 app.set('views', __dirname+'/views');
@@ -23,6 +24,6 @@ app.get('/about-us', function(req, res){
   });
 });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log('Server is running on port 3000');
 });
